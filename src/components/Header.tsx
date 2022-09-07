a0ac0728f19bbfe1f347a7e4988f1048cb748459
NaokiTateyama <tateyamanaoki@lelab.t.u-tokyo.ac.jp>
2022-09-07 20:27:15 +0900

    Fix lag of Menu component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,13 +8,13 @@ import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
 
 import { jumpTo, ID } from 'utils/JumpTo';
 
 type HeaderProps = {
   isPC: boolean
 }
+
 function Header(props: HeaderProps){
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
@@ -32,39 +32,47 @@ function Header(props: HeaderProps){
   const handleClose = () => {
     setAnchorEl(null);
   };
+
   return (
     <>
       {!props.isPC && (
-        <AppBar position='sticky' sx={{top: 0, backgroundColor: 'transparent'}}>
-        <Toolbar>
-          <div style={{ flexGrow: 1 }}></div>
-          <IconButton
-            sx={{ justifyContent: 'flex-end'}}
-            size='large'
-            aria-controls='menu-appbar'
-            aria-haspopup='true'
-            onClick={handleMenu}
-            color='inherit'>
-            
-            {anchorEl==null ? <MenuIcon /> : <CloseIcon/>}
-          </IconButton>
-          <Menu
-            id='menu-appbar'
-            PaperProps={{
-              style: { width: '100%' }
-            }}
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'right',
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            open={Boolean(anchorEl)}
-            onClose={handleClose}>
+        <AppBar
+          position='sticky'
+          sx={{top: 0, backgroundColor: 'transparent', backgroundImage: 'none !important', boxShadow: 'none !important'}}>
+          <Toolbar>
+            <div style={{ flexGrow: 1 }}></div>
+            <IconButton
+              sx={{ justifyContent: 'flex-end'}}
+              size='large'
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              onClick={handleMenu}
+              color='inherit'>
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id='menu-appbar'
+              PaperProps={{
+                style: { width: '100%' }
+              }}
+              BackdropProps={{
+                style: {backgroundColor: 'rgba(1,1,1,0.6)'}
+              }}
+              TransitionProps={{
+                timeout: 0
+              }}
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}>
               <MenuItem sx={{ justifyContent: 'center', fontSize: '1.2rem', margin: 2 }} onClick={()=>handleJump(ID.JOB_EXPERIENCE)}>Job experience</MenuItem>
               <MenuItem sx={{ justifyContent: 'center', fontSize: '1.2rem', margin: 2 }} onClick={()=>handleJump(ID.EDUCATION)}>Education</MenuItem>
               <MenuItem sx={{ justifyContent: 'center', fontSize: '1.2rem', margin: 2 }} onClick={()=>handleJump(ID.PUBLICATIONS)}>Publications</MenuItem>
@@ -73,7 +81,7 @@ function Header(props: HeaderProps){
           </Toolbar>
         </AppBar>
       )}
-      <Box style={{textAlign: 'center'}} mt={3} mb={3}>
+      <Box style={{textAlign: 'center'}} mt={props.isPC ? 3:0} mb={props.isPC ? 3:0}>
         <Typography variant='h2' gutterBottom>立山 尚樹</Typography>
         <Typography variant='h2' gutterBottom>Naoki Tateyama</Typography>
         <Box>
