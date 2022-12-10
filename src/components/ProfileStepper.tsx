da10da9d51cb0035e0d57e0f4b552f8ea2fe7152
NaokiTateyama <tateyamanaoki@lelab.t.u-tokyo.ac.jp>
2022-12-10 23:40:25 +0900

    fix ProfileStepper

--- src/components/ProfileStepper.tsx
+++ src/components/ProfileStepper.tsx
@@ -10,53 +10,43 @@ import { styled } from '@mui/material/styles';
 import { BIO_ORDER } from 'Information';
 import { jumpTo } from 'utils/JumpTo';
 
-type StepType = {
-  id: string;
-  label: string | JSX.Element;
-  description?: Array<JSX.Element>;
-};
-
-const steps = BIO_ORDER.map((bio, i) => {
-  return {
-    id: bio.id,
-    label: <Typography style={{ cursor: 'pointer' }}>{bio.title}</Typography>,
-    description: bio.children?.map((child) => {
-      return (
-        <Typography
-          key={`description-${bio.id}-${child.id}-${i}`}
-          style={{ cursor: 'pointer' }}
-          onClick={() => jumpTo(child.id)}
-          gutterBottom
-        >
-          {child.title}
-        </Typography>
-      );
-    })
-  };
-});
-
 const CustomStepContent = styled(StepContent)({
   '&.MuiStepContent-root': { borderLeft: '1px solid #757575' }
 });
 
 function ProfileStepper() {
   const [activeStep, setActiveStep] = useState(0);
-  const onClickStep = (i: number, step: StepType) => {
+  const onClickStep = (i: number, id: string) => {
     setActiveStep(i);
-    jumpTo(step.id);
+    jumpTo(id);
   };
 
   return (
     <Box>
       <Box sx={{ minWidth: 200, top: '20px', position: 'sticky' }} mr={2}>
         <Stepper activeStep={activeStep} orientation="vertical">
-          {steps.map((step, i) => {
+          {BIO_ORDER.map((bio, i) => {
             return (
               <Step key={`step-${i}`} active={true}>
-                <StepLabel icon={<></>} onClick={() => onClickStep(i, step)}>
-                  {step.label}
+                <StepLabel icon={<></>} onClick={() => onClickStep(i, bio.id)}>
+                  <Typography style={{ cursor: 'pointer' }}>
+                    {bio.title}
+                  </Typography>
                 </StepLabel>
-                <CustomStepContent>{step.description}</CustomStepContent>
+                <CustomStepContent>
+                  {bio.children?.map((child) => {
+                    return (
+                      <Typography
+                        key={`description-${bio.id}-${child.id}-${i}`}
+                        style={{ cursor: 'pointer' }}
+                        onClick={() => jumpTo(child.id)}
+                        gutterBottom
+                      >
+                        {child.title}
+                      </Typography>
+                    );
+                  })}
+                </CustomStepContent>
               </Step>
             );
           })}
@@ -65,4 +55,5 @@ function ProfileStepper() {
     </Box>
   );
 }
+
 export default ProfileStepper;
